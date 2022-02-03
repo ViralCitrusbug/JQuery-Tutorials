d6fd44af674bc8e530a2002a255e4e422d412284
Viral <viral.citrusbug@gmail.com>
2022-02-03 14:21:38 +0530

    Jquery Tutorial almost complted

--- js/main.js
+++ js/main.js
@@ -1,11 +1,71 @@
+
 $(document).ready(function () {
 
-    $('p').click(
-        function () {
-            $('p').hide()
-        }
-    )  
+ 
+    $('p').on(
+        {
+            click: function () {
+                console.log('Thanks for clicking', this);
+            },
+            mouseleave: function () {
+                console.log("mouseleave");
+
+            }
 
 
-// document.ready ends here 
-})
+
+        })
+
+    // $('#wiki').hide(1000, function () {
+    //     console.log("hidden");
+    // })   
+    // $('#wiki').show(1000, function () {
+    //     console.log("show");
+    // })  
+    // $('#but').click(function () {
+    //     $('#wiki').fadeOut(5000);
+    // })
+
+    //     fadeIn()
+    // fadeOut()
+    // fadeToggle()
+    // fadeTo()
+
+    // Slide methods - speed and callback parameters are optional
+    // $('#wiki').slideUp(1000, function(){
+    //     console.log('done');
+    // })
+    // $('#wiki').slideDown(1000)
+    // $('#wiki').slideToggle(1000)
+
+    // Animate function in jQuery
+    // $('#wiki').animate({
+    //     opacity:0.3,
+    //     height: '150px',
+    //     width:'350px'
+
+    // }, "fast")
+    // $('#wiki').animate({ opacity: 0.3 }, 4000);
+    // $('#wiki').animate({ opacity: 0.9 }, 1000);
+    // $('#wiki').animate({ width: '350px' }, 12000);
+
+    // $('#ta').val('setting it to harry');
+    // $('#ta').html('setting it to harry');
+    // $('#ta').html('setting it to harry3');
+    // $('#inp').html('setting it to harry3');
+    // $('#inp').val('setting it to harry3');
+    // $('#inp').empty()
+    // $('#wiki').empty()
+    // $('#wiki').text('you are good')
+    // $('#wiki').remove()
+
+    // $('#wiki').addClass('myclass')
+    // $('#wiki').addClass('myclass2')
+    // $('#wiki').removeClass('myclass2')
+    // $('#wiki').css('background-color', 'red')
+    // $('#wiki').css('background-color')
+
+
+});
+
+
